a553c4fc1dd95a4551eee78589ddaf6c7ede9455
MD SANAULLAH <alamsanaullah57@gmail.com>
2026-02-19 20:47:20 +0530

    get transcript from url

--- get_transcript.py
+++ get_transcript.py
@@ -1,8 +1,10 @@
+# getting transcript of youtube video using youtube-api
 from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import JSONFormatter
 
-yt = 'https://youtu.be/DFwppvrL_pE?si=wO5NE7e5jajBvXsX'
 
 def fetch_transcript(url):
+  # extracting video_id from the given url.
   def extract_video_id(url: str):
     video_id = url.split("?")[0].split("/")[-1]
     return video_id
@@ -10,18 +12,17 @@ def fetch_transcript(url):
   
   try:
     yt_api = YouTubeTranscriptApi()
-    transcript = yt_api.fetch(video_id)
-    
-    
-    
-    
-    pass
+    formatter = JSONFormatter()
+    raw_transcript = yt_api.fetch(video_id)
+    transcript = formatter.format_transcript(transcript=raw_transcript)
+    return transcript
+  
   except Exception as e:
     print(f"Facing Error: {e}")
 
 
+if __name__== "__main__":
 
-
-
-
-# fetch_transcript(yt)
+  yt = 'https://youtu.be/DFwppvrL_pE?si=wO5NE7e5jajBvXsX'
+  transcript = fetch_transcript(yt)
+  print(transcript)
