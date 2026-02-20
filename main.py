31d5e7f48a755df0354c5e3a601006e9cb2f98c7
MD SANAULLAH <alamsanaullah57@gmail.com>
2026-02-21 03:47:19 +0530

    try and except

--- main.py
+++ main.py
@@ -36,31 +36,34 @@ class YoutubeRagAssistant:
   # 2. group_chunk_num(int): number of chunk to be grouped into one.'''
 
   def __merge_chunks(self, transcript: list[dict], group_chunk_num: int= 6):
-    new_chunk = []
-    no_chunk = len(transcript)
-    # num of groups of chunk possible
-    num_groups = math.ceil(no_chunk/group_chunk_num)
+    try:  
+      new_chunk = []
+      no_chunk = len(transcript)
+      # num of groups of chunk possible
+      num_groups = math.ceil(no_chunk/group_chunk_num)
 
-    for i in range(num_groups):
-      # start and end index of chunk
-      start_indx = i*group_chunk_num
-      end_indx = min((i+1)*group_chunk_num, no_chunk)
-      # group of chunks
-      chunk_group = transcript[start_indx:end_indx]
-      # refining the text format(removing \n from text)
-      text = " ".join(c["text"] for c in chunk_group).replace("\n", " ")
-      # adding metadata to new_chunk
-      new_chunk.append({
-        "text": text,
-        "start": chunk_group[0]["start"],
-        "duration": round((chunk_group[-1]["start"] - chunk_group[0]["start"]), 3) if start_indx!=end_indx else 1
-      })
-    
-    # saving the content of video for creating summary.
-    summary = " ".join(chunk["text"] for chunk in new_chunk)
-    # saving new_chunk as json
-    
-    return new_chunk, summary
+      for i in range(num_groups):
+        # start and end index of chunk
+        start_indx = i*group_chunk_num
+        end_indx = min((i+1)*group_chunk_num, no_chunk)
+        # group of chunks
+        chunk_group = transcript[start_indx:end_indx]
+        # refining the text format(removing \n from text)
+        text = " ".join(c["text"] for c in chunk_group).replace("\n", " ")
+        # adding metadata to new_chunk
+        new_chunk.append({
+          "text": text,
+          "start": chunk_group[0]["start"],
+          "duration": round((chunk_group[-1]["start"] - chunk_group[0]["start"]), 3) if start_indx!=end_indx else 1
+        })
+      
+      # saving the content of video for creating summary.
+      summary = " ".join(chunk["text"] for chunk in new_chunk)
+      # saving new_chunk as json
+      
+      return new_chunk, summary
+    except Exception as e:
+      print(f"Error occured: {e}")
   
   #-----------------x
   # 1. Make array of chunks.
@@ -69,65 +72,75 @@ class YoutubeRagAssistant:
   # 4. save new json data or update the previous json and use it.
   # Parameter: Json data
   def __create_embeddings(self, transcript: list[dict]):
-    arr_text = [chunk["text"] for chunk in transcript]
+    try:  
+      arr_text = [chunk["text"] for chunk in transcript]
 
-    def ollama_embed(arr_text):
-      response = ollama.embed(
-        model="nomic-embed-text",
-        input=arr_text)
-      return response.embeddings
-    
-    embeddings = ollama_embed(arr_text) # It's array of embeddings
+      def ollama_embed(arr_text):
+        response = ollama.embed(
+          model="nomic-embed-text",
+          input=arr_text)
+        return response.embeddings
+      
+      embeddings = ollama_embed(arr_text) # It's array of embeddings
 
-    for i, chunk in enumerate(transcript):
-      chunk["embedding"] = embeddings[i]
+      for i, chunk in enumerate(transcript):
+        chunk["embedding"] = embeddings[i]
 
-    return transcript
+      return transcript
+    except Exception as e:
+      print(f"Error: {e}")
 
   #----------------------X
   # 1. get query from user
   # 2. using Cosine Similarity find the most relevant chunk(2 or more)
   # 3. make a prompt and feed to LLM with relevant chunk and query.
   def __fetch_relevant_chunk(self, user_query: str, dataframe, top_result=3):
-    def embed_query(user_query):
-      response = ollama.embed(
-        model="nomic-embed-text",
-        input=[user_query])
+    try:  
+      def embed_query(user_query):
+        response = ollama.embed(
+          model="nomic-embed-text",
+          input=[user_query])
+        
+        return response.embeddings[0]
       
-      return response.embeddings[0]
-    
-    embedded_query = embed_query(user_query)
+      embedded_query = embed_query(user_query)
 
-    similarity = cosine_similarity(np.vstack(dataframe["embedding"]), [embedded_query]).flatten() # type: ignore
+      similarity = cosine_similarity(np.vstack(dataframe["embedding"]), [embedded_query]).flatten() # type: ignore
 
-    top_similar_indx = similarity.argsort()[-top_result:][::-1]
+      top_similar_indx = similarity.argsort()[-top_result:][::-1]
 
-    relevant_chunk = dataframe.loc[top_similar_indx][["text", "start", "duration"]]
+      relevant_chunk = dataframe.loc[top_similar_indx][["text", "start", "duration"]]
 
-    return relevant_chunk.to_json(orient="records")
+      return relevant_chunk.to_json(orient="records")
 
+    except Exception as e:
+      print(f"ERROR: {e}")
+  
   # getting the response from LLM-Agentss
   def __LLM_response(self, query, metadata, summary):
-    prompt = f'''
-    note: Given Below is a query and some data(contains- start time in seconds, duration in seconds, and text)
-    and you have to answer the query using the data and mention the time stamp after converting seconds into minute(example if start time in seconds is 60.000 it will be  1:00 in minutes)
-    # if query says to summarise then give the short summary in 100-150 words using this:[{summary}] 
-    #important message: don't ask any question at end.
+    try: 
+      prompt = f'''
+      note: Given Below is a query and some data(contains- start time in seconds, duration in seconds, and text)
+      and you have to answer the query using the data and mention the time stamp after converting seconds into minute(example if start time in seconds is 60.000 it will be  1:00 in minutes)
+      # if query says to summarise then give the short summary in 100-150 words using this:[{summary}] 
+      #important message: don't ask any question at end.
 
-    query: {query}
+      query: {query}
 
-    data: {metadata}'''
-    
-    client = genai.Client(api_key=API_KEY)
-    response = client.models.generate_content(
-      model="gemini-3-flash-preview",
-      contents=prompt)
-    
-    return response.text
+      data: {metadata}'''
+      
+      client = genai.Client(api_key=API_KEY)
+      response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents=prompt)
+      
+      return response.text
 
+    except Exception as e:
+      print(f"Error: {e}")
 
   # save dataframe to use it without making it again and again
-  def initiate_dataframe(self, url):
+  def __initiate_dataframe(self, url: str):
     # fetch transcript
     transcript = self.__fetch_transcript(url)
     # merge chunks of transcript and get summary
@@ -135,12 +148,11 @@ class YoutubeRagAssistant:
     # create embeddings of chunk in transcript
     transcript = self.__create_embeddings(transcript)
     
-    dataframe = pd.DataFrame(transcript)
-    return dataframe
-    
-  
+    return pd.DataFrame(transcript), summary
 
 
+  def relevant_metadata(self, query, dataframe, top_result=3):
+    pass
 
-  def youtube_rag_assistant(self):
+  def gen_LLM_response(self, query, metadata):
     pass
