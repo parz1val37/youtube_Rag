ccb2dd5dd98d0aac6853ba64fe7aa25d4a5d2afb
MD SANAULLAH <alamsanaullah57@gmail.com>
2026-02-21 03:48:28 +0530

    final

--- all_functions/merge_chunks.py
+++ all_functions/merge_chunks.py
@@ -6,28 +6,31 @@ import math
 # 2. group_chunk_num(int): number of chunk to be grouped into one.'''
 
 def merge_chunks(transcript: list[dict], group_chunk_num: int= 6):
-  new_chunk = []
-  no_chunk = len(transcript)
-  # num of groups of chunk possible
-  num_groups = math.ceil(no_chunk/group_chunk_num)
+  try:
+    new_chunk = []
+    no_chunk = len(transcript)
+    # num of groups of chunk possible
+    num_groups = math.ceil(no_chunk/group_chunk_num)
 
-  for i in range(num_groups):
-    # start and end index of chunk
-    start_indx = i*group_chunk_num
-    end_indx = min((i+1)*group_chunk_num, no_chunk)
-    # group of chunks
-    chunk_group = transcript[start_indx:end_indx]
-    # refining the text format(removing \n from text)
-    text = " ".join(c["text"] for c in chunk_group).replace("\n", " ")
-    # adding metadata to new_chunk
-    new_chunk.append({
-      "text": text,
-      "start": chunk_group[0]["start"],
-      "duration": round((chunk_group[-1]["start"] - chunk_group[0]["start"]), 3) if start_indx!=end_indx else 1
-    })
-  
-  # saving the content of video for creating summary.
-  summary = " ".join(chunk["text"] for chunk in new_chunk)
-  # saving new_chunk as json
-  
-  return new_chunk, summary
+    for i in range(num_groups):
+      # start and end index of chunk
+      start_indx = i*group_chunk_num
+      end_indx = min((i+1)*group_chunk_num, no_chunk)
+      # group of chunks
+      chunk_group = transcript[start_indx:end_indx]
+      # refining the text format(removing \n from text)
+      text = " ".join(c["text"] for c in chunk_group).replace("\n", " ")
+      # adding metadata to new_chunk
+      new_chunk.append({
+        "text": text,
+        "start": chunk_group[0]["start"],
+        "duration": round((chunk_group[-1]["start"] - chunk_group[0]["start"]), 3) if start_indx!=end_indx else 1
+      })
+    
+    # saving the content of video for creating summary.
+    summary = " ".join(chunk["text"] for chunk in new_chunk)
+    # saving new_chunk as json
+    
+    return new_chunk, summary
+  except Exception as e:
+    print(f"Error occured: {e}")
